086eaf9507684ac965d9bfd3f571250114127b06
kenny <kennychaos7@gmail.com>
2021-01-08 16:24:04 +0800

    1. 增加diff功能(还需要测试）

--- EasyLibrary/src/main/java/com/easy/recyclerview/EasyAdapter.java
+++ EasyLibrary/src/main/java/com/easy/recyclerview/EasyAdapter.java
@@ -6,6 +6,9 @@ import android.view.ViewGroup;
 
 import androidx.annotation.LayoutRes;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListUpdateCallback;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
@@ -19,12 +22,14 @@ import java.util.List;
  * @Description:    如果要做VH为<? extentds EasyViewHolder>的话需要做类型擦除, 然后取出基类EasyViewHolder
  */
 public abstract class EasyAdapter<T> extends RecyclerView.Adapter<EasyViewHolder> {
+    private EasyAdapter<T> adapter = null;
     private @LayoutRes int itemLayoutId = -1;
     private EasyItemListener itemListener = null;
 //    private EasyViewHolder viewHolder = null;
     private final List<T> arrayList = Collections.synchronizedList(new ArrayList<>());//CopyOnWriteArrayList
 
     public EasyAdapter(@LayoutRes int itemLayoutId,@NonNull EasyAdapter.EasyItemListener listener) {
+        adapter = this;
         this.itemLayoutId = itemLayoutId;
         this.itemListener = listener;
     }
@@ -57,12 +62,58 @@ public abstract class EasyAdapter<T> extends RecyclerView.Adapter<EasyViewHolder
     }
 
     /**
-     * 更新数据
-     * TODO 利用DiffUtil处理数据
+     * 利用DiffUtil处理数据
      * @param newData
+     * @param callback
      */
-    public void diffUpdate(List<T> newData) {
+    public void diffUpdate(List<T> newData, DiffVerifyCallback<T> callback) {
+        if (arrayList.size() > 0 && newData.size() > 0) {
+            DiffUtil.DiffResult diffResult = DiffUtil.calculateDiff(new DiffUtil.Callback() {
+                @Override
+                public int getOldListSize() {
+                    return arrayList.size();
+                }
 
+                @Override
+                public int getNewListSize() {
+                    return newData.size();
+                }
+
+                @Override
+                public boolean areItemsTheSame(int oldItemPosition, int newItemPosition) {
+                    return callback.isSameItem(arrayList.get(oldItemPosition), newData.get(newItemPosition));
+                }
+
+                @Override
+                public boolean areContentsTheSame(int oldItemPosition, int newItemPosition) {
+                    return callback.isSameContent(arrayList.get(oldItemPosition), newData.get(newItemPosition));
+                }
+            }, false);
+            diffResult.dispatchUpdatesTo(new ListUpdateCallback() {
+                @Override
+                public void onInserted(int position, int count) {
+                    adapter.notifyItemRangeInserted(position, count);
+                }
+
+                @Override
+                public void onRemoved(int position, int count) {
+                    adapter.notifyItemRangeRemoved(position, count);
+                }
+
+                @Override
+                public void onMoved(int fromPosition, int toPosition) {
+                    adapter.notifyItemMoved(fromPosition, toPosition);
+                }
+
+                @Override
+                public void onChanged(int position, int count, @Nullable Object payload) {
+                    adapter.notifyItemRangeChanged(position, count, payload);
+                }
+            });
+        }
+        else if (arrayList.size() == 0 && newData.size() > 0) {
+            addNewData(newData);
+        }
     }
 
     public void replace(int position, T t) {
@@ -105,5 +156,10 @@ public abstract class EasyAdapter<T> extends RecyclerView.Adapter<EasyViewHolder
 
     public interface EasyItemListener {
         void onItemClick(int position, View view);
+    }
+
+    public interface DiffVerifyCallback<T> {
+        boolean isSameItem(T _old, T _new);
+        boolean isSameContent(T _old, T _new);
     }
 }
--- app/src/main/java/com/example/easyrecyclerview/SecondFragment.java
+++ app/src/main/java/com/example/easyrecyclerview/SecondFragment.java
@@ -1,10 +1,12 @@
 package com.example.easyrecyclerview;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -43,7 +45,8 @@ public class SecondFragment extends Fragment {
             }
         });
 
-        initTestEasyRecyclerView(view.findViewById(R.id.framelayout));
+//        initTestEasyRecyclerView(view.findViewById(R.id.framelayout));
+        initTestBeanEasyRecyclerView(view.findViewById(R.id.framelayout));
     }
 
     private void initTestEasyRecyclerView(ViewGroup viewGroup) {
@@ -51,17 +54,13 @@ public class SecondFragment extends Fragment {
         for (int i = 0; i < 50; i ++) {
             stringList.add(new StringBuffer().append(Math.random()).append(" - ").append(i).toString());
         }
-        /*
-        TestAdapter testAdapter = new TestAdapter();
-        testAdapter.setData(stringList);
-        */
         TestAdapter testAdapter = new TestAdapter(R.layout.item_test_easy_recyclerview, new EasyAdapter.EasyItemListener() {
             @Override
             public void onItemClick(int position, View view) {
                 Log.e(":", position + " view " + view.getClass().getSimpleName());
             }
         });
-        EasyRecyclerView easyRecyclerView = new EasyRecyclerView.Builder(getContext())
+        EasyRecyclerView.Builder easyRecyclerViewBuilder = new EasyRecyclerView.Builder(getContext())
                 .setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false))
                 .setAdapter(testAdapter)
                 .setAllowLoadMore(true)
@@ -128,47 +127,130 @@ public class SecondFragment extends Fragment {
                     public void onFinish(boolean isSuccess, View view) {
 
                     }
-                })
-                .build();
+                });
+        EasyRecyclerView easyRecyclerView = easyRecyclerViewBuilder.build();
         viewGroup.addView(
                         easyRecyclerView.toView()
         );
         testAdapter.setNewData(stringList);
     }
 
-    /*
-    private class TestVH extends RecyclerView.ViewHolder{
-        public TestVH(@NonNull View itemView) {
-            super(itemView);
+    //diff
+    //  鉴于需要双倍list的内存大小才能匹配, 并不建议使用
+    private void initTestBeanEasyRecyclerView(ViewGroup viewGroup) {
+        TestBeanAdapter testBeanAdapter = new TestBeanAdapter(R.layout.item_test_easy_recyclerview, new EasyAdapter.EasyItemListener() {
+            @Override
+            public void onItemClick(int position, View view) {
+
+            }
+        });
+        EasyRecyclerView.Builder easyRecyclerViewBuilder = new EasyRecyclerView.Builder(getContext())
+                .setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false))
+                .setAdapter(testBeanAdapter)
+                .setAllowLoadMore(true)
+                .setAllowRefresh(true)
+                .setClassicsHeader(new BaseEasyHeader.BaseOnRefreshListener() {
+                    @Override
+                    public void onPullingToRefresh(View v) {
+
+                    }
+
+                    @Override
+                    public void onRefreshStart(View v) {
+
+                    }
+
+                    @Override
+                    public void onRefreshing(View v) {
+
+                    }
+
+                    @Override
+                    public void onFinish(boolean isSuccess, View view) {
+
+                    }
+                })
+                .setFooter(EasyRecyclerViewUtils.getInstance().createFooter(getContext(), R.layout.footer_test, false, new BaseEasyFooter.BaseOnLoadMoreListener() {
+                    @Override
+                    public void onPullingToLoad(View v) {
+
+                    }
+
+                    @Override
+                    public void onLoadMoreStart(View v) {
+
+                    }
+
+                    @Override
+                    public void onLoading(View v) {
+
+                    }
+
+                    @Override
+                    public void onFinish(boolean isSuccess, View view) {
+
+                    }
+                }))
+                .setClassicsFooter(new BaseEasyFooter.BaseOnLoadMoreListener() {
+                    @Override
+                    public void onPullingToLoad(View v) {
+
+                    }
+
+                    @Override
+                    public void onLoadMoreStart(View v) {
+
+                    }
+
+                    @Override
+                    public void onLoading(View v) {
+
+                    }
+
+                    @Override
+                    public void onFinish(boolean isSuccess, View view) {
+
+                    }
+                });
+        EasyRecyclerView easyRecyclerView = easyRecyclerViewBuilder.build();
+        List<TestBean> testBeanList = new ArrayList<>();
+        for (int i = 0; i < 50; i ++) {
+            testBeanList.add(new TestBean(i + "0", " - " + i, i%2));
         }
+        viewGroup.addView(
+                easyRecyclerView.toView()
+        );
+        testBeanAdapter.addNewData(testBeanList);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                List<TestBean> testBeanList2 = new ArrayList<>();
+                for (int i = 0; i < 30; i ++) {
+                    testBeanList2.add(new TestBean(i + "/0", " - " + i, i));
+                }
+                testBeanAdapter.diffUpdate(testBeanList2, new EasyAdapter.DiffVerifyCallback<TestBean>() {
+                    @Override
+                    public boolean isSameItem(TestBean _old, TestBean _new) {
+                        if (_old.index == _new.index) {
+                            Toast.makeText(getActivity(),"isSameItem true " + _new.name, Toast.LENGTH_SHORT).show();
+                            return true;
+                        }
+                        return false;
+                    }
+
+                    @Override
+                    public boolean isSameContent(TestBean _old, TestBean _new) {
+                        if (_old.value.equals(_new.value)) {
+                            Toast.makeText(getActivity(), "isSameContent true " + _new.name, Toast.LENGTH_SHORT).show();
+                            return true;
+                        }
+                        return false;
+                    }
+                });
+            }
+        },10 * 1000);
     }
-
-    private class TestAdapter extends RecyclerView.Adapter<TestVH> {
-        private List<String> stringList = new ArrayList<>();
-
-        public void setData(List<String> stringList) {
-            this.stringList = stringList;
-            notifyDataSetChanged();
-        }
-
-        @NonNull
-        @Override
-        public TestVH onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            return new TestVH(
-                    LayoutInflater.from(parent.getContext()).inflate(R.layout.item_test_easy_recyclerview, null)
-            );
-        }
-
-        @Override
-        public void onBindViewHolder(@NonNull TestVH holder, int position) {
-            ((TextView)holder.itemView.findViewById(R.id.textview)).setText(stringList.get(position));
-        }
-
-        @Override
-        public int getItemCount() {
-            return stringList.size();
-        }
-    }*/
 
     private class TestAdapter extends EasyAdapter<String>  {
 
@@ -179,6 +261,36 @@ public class SecondFragment extends Fragment {
         @Override
         public void onEasyBindViewHolder(@NonNull EasyViewHolder holder, int position) {
             holder.setText(R.id.textview, getData(position));
+        }
+    }
+
+    private class TestBeanAdapter extends EasyAdapter<TestBean> {
+
+        public TestBeanAdapter(int itemLayoutId, @NonNull EasyItemListener listener) {
+            super(itemLayoutId, listener);
+        }
+
+        @Override
+        public void onEasyBindViewHolder(@NonNull EasyViewHolder holder, int position) {
+            holder.setText(R.id.textview,
+                    getData(position).name
+                            + " -- " + getData(position).value
+                            + " -- " + getData(position).index
+                            + " -- " + getData(position).time);
+        }
+    }
+
+    private class TestBean{
+        private String name;
+        private String value;
+        private long time;
+        private int index;
+
+        public TestBean(String name, String value, int index) {
+            this.name = name;
+            this.value = value;
+            this.index = index;
+            this.time = System.currentTimeMillis();
         }
     }
 }
